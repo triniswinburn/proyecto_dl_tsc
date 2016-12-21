b6d8b4a5fc292103026ab983bf152007af1b37c4
Trini Swinburn <trini.swinburn@gmail.com>
2016-12-20 21:51:00 -0300

    sign out and strong params

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, controllers: {
+        registrations: 'users/registrations'
+      }
+
   root to: 'public#index'
 
   devise_for :admin_users, ActiveAdmin::Devise.config
--- db/seeds.rb
+++ db/seeds.rb
@@ -13,4 +13,19 @@
 # logged_user (4)
 # users
 # houses (16)
+
 AdminUser.create!(email: 'trini.swinburn@gmail.com', password: '12345678', password_confirmation: '12345678')
+
+
+# Company.destroy_all
+
+# category = Company.create(
+# 	[
+# 		{ name: 'company1', email: 'company1@gmail.com', address: 'direccion company 1', phone: '+562 2233 4452', photo: 'foto company 1', user_id: }, 
+# 		{ name: 'company2', email: 'company2@gmail.com', address: 'direccion company 2', phone: '+562 1245 4452', photo: 'foto company 2', user_id },
+# 		{ name: 'company3', email: 'company3@gmail.com', address: 'direccion company 3', phone: '+562 2233 7443', photo: 'foto company 3', user_id },
+# 		{ name: 'company4', email: 'company4@gmail.com', address: 'direccion company 4', phone: '+562 8374 6652', photo: 'foto company 4', user_id }
+# 	]
+# )
+
+
