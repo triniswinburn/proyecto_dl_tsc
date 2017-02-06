bef10615c8bf30bb7e7229e962e4b2ec85192649
Trini Swinburn <trini.swinburn@gmail.com>
2017-02-06 19:34:07 -0300

    cambio de idioma de frases

--- app/admin/company.rb
+++ app/admin/company.rb
@@ -3,7 +3,6 @@ ActiveAdmin.register Company do
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
 #
-#
 # or
 #
 # permit_params do
